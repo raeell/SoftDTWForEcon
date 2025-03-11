ccbb6d3511417f2c48bcb32333f27e22b3354d7c
tnguyen <tuduyen.nguyen@ensae.fr>
2025-03-11 09:56:41 +0000

    Add metrics file, clean up, update documentation

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # SoftDTWForEcon
 Dépôt pour le projet final de l'évaluation du cours Mise en production (ENSAE 3A).
 
-```
+```python
 # Install dependencies in venv
 python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-# Clone repository with soft-dtw implementation
-git clone https://github.com/google-research/soft-dtw-divergences.git
+# Initialize submodule for soft-dtw divergence calculation
+git submodule init
+git submodule update
+git apply --directory=soft-dtw-divergences/ patches/softdtw.patch
 cd soft-dtw-divergences
 python setup.py install
 cd ..
-git apply --directory=soft-dtw-divergences/ patches/softdtw.patch
 # Create Jupyter kernel for venv
 python -m ipykernel install --user --name .venv
 ```
+
+Le code du projet est pour l'instant dans un notebook (`notebooks/projet_time_series.ipynb`). Les métriques sont dans `src/metrics.py`
--- src/metrics.py
+++ src/metrics.py
@@ -1,1 +1,73 @@
 from sdtw_div.numba_ops import *
+
+
+class SoftDTW:
+    def __init__(self, gamma):
+        super(SoftDTW, self).__init__()
+        self.gamma = gamma
+
+    def __call__(self, x, y):
+        return sdtw(x, y, gamma=self.gamma)
+
+
+class SoftDTWValueAndGrad:
+    def __init__(self, gamma):
+        super(SoftDTWValueAndGrad, self).__init__()
+        self.gamma = gamma
+
+    def __call__(self, x, y):
+        return sdtw_value_and_grad(x, y, gamma=self.gamma)
+
+
+class SharpSoftDTW:
+    def __init__(self, gamma):
+        super(SharpSoftDTW, self).__init__()
+        self.gamma = gamma
+
+    def __call__(self, x, y):
+        return sharp_sdtw(x, y, gamma=self.gamma)
+
+
+class SharpSoftDTWValueAndGrad:
+    def __init__(self, gamma):
+        super(SharpSoftDTWValueAndGrad, self).__init__()
+        self.gamma = gamma
+
+    def __call__(self, x, y):
+        return sharp_sdtw_value_and_grad(x, y, gamma=self.gamma)
+
+
+class SoftDTWDiv:
+    def __init__(self, gamma):
+        super(SoftDTWDiv, self).__init__()
+        self.gamma = gamma
+
+    def __call__(self, x, y):
+        return sdtw_div(x, y, gamma=self.gamma)
+
+
+class SoftDTWDivValueAndGrad:
+    def __init__(self, gamma):
+        super(SoftDTWDivValueAndGrad, self).__init__()
+        self.gamma = gamma
+
+    def __call__(self, x, y):
+        return sdtw_div_value_and_grad(x, y, gamma=self.gamma)
+
+
+class SharpSoftDTWDiv:
+    def __init__(self, gamma):
+        super(SharpSoftDTWDiv, self).__init__()
+        self.gamma = gamma
+
+    def __call__(self, x, y):
+        return sharp_sdtw_div(x, y, gamma=self.gamma)
+
+
+class SharpSoftDTWDivValueAndGrad:
+    def __init__(self, gamma):
+        super(SharpSoftDTWDivValueAndGrad, self).__init__()
+        self.gamma = gamma
+
+    def __call__(self, x, y):
+        return sharp_sdtw_div_value_and_grad(x, y, gamma=self.gamma)

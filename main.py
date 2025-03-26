af68edac345b89eaf0a8b5bee22db0fad8e18555
laurinemir <laurinemeier@gmail.com>
2025-03-26 14:25:59 +0000

    linter

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
+"""
+Main script for data processing and model training.
+
+This script loads data from S3, preprocesses it, and trains a model.
+"""
+import os
+
 import pandas as pd
 import torch
-import os
-from model.train_model import train_models_insee
-from model.eval_model import eval_models_insee, error_insee
-from model.forecast_model import plot_forecasts_insee
 import matplotlib.pyplot as plt
 import s3fs
 from dotenv import load_dotenv
 
+from model.train_model import train_models_insee
+from model.eval_model import eval_models_insee
+from model.forecast_model import plot_forecasts_insee
 
 load_dotenv()
 
 
 MY_BUCKET = os.getenv("MY_BUCKET", "laurinemir")
-path = f"s3://{MY_BUCKET}/taxi_data/"
+PATH = f"s3://{MY_BUCKET}/taxi_data/"
+INPUT_SIZE = 20
+OUTPUT_SIZE = 5
+
+
+if torch.cuda.is_available():
+    DEV = "cuda:0"
+else:
+    DEV= "cpu"
+device = torch.device(DEV)
+
 
 fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": "https://minio.lab.sspcloud.fr"})
-files = fs.ls(path)
+files = fs.ls(PATH)
 
 dfs = []
 for file in files:
@@ -27,16 +43,18 @@ for file in files:
 df = pd.concat(dfs, ignore_index=True)
 print(df.head())
 
-#df = pd.read_csv("DS_ICA_CSV_FR/DS_ICA_data.csv", sep=";", encoding="utf-8")
+# df = pd.read_csv("DS_ICA_CSV_FR/DS_ICA_data.csv", sep=";", encoding="utf-8")
 # df["TIME_PERIOD"] = pd.to_datetime(df["TIME_PERIOD"], format="%Y-%m")
 
 
-df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"], format="%Y-%m-%d %H:%M:%S")
+df["tpep_pickup_datetime"] = pd.to_datetime(
+    df["tpep_pickup_datetime"], format="%Y-%m-%d %H:%M:%S"
+)
 df["hour"] = df["tpep_pickup_datetime"].dt.floor("h")
 df_activity = df.groupby("hour").size().reset_index(name="num_trips")
 df_activity = df_activity[df_activity["num_trips"] >= 100]
 df_activity["hour"] = pd.to_datetime(df_activity["hour"])
-print(df_activity.head(n=10)) 
+print(df_activity.head(n=10))
 
 
 # colonne = df.columns[0]  # colonne Activite
@@ -49,24 +67,18 @@ print(df_activity.head(n=10))
 # )  # choisir le secteur activité et indicateur
 
 plt.figure(figsize=(12, 6))  # Définir la taille du graphique
-plt.plot(df_activity["hour"], df_activity["num_trips"],marker='o', label="num_trips")  
+plt.plot(df_activity["hour"], df_activity["num_trips"], marker="o", label="num_trips")
 plt.xlabel("Date")
 plt.ylabel("num_trips")
 plt.title("Évolution de num_trips dans le temps")
 plt.grid()
 plt.legend()
 plt.show()
-plt.savefig("plots/output_taxi.png") 
+plt.savefig("plots/output_taxi.png")
 
 
-input_size = 100
-output_size = 5
 
-if torch.cuda.is_available():
-    dev = "cuda:0"
-else:
-    dev = "cpu"
-device = torch.device(dev)
+
 
 
 gammas = [1]
@@ -76,8 +88,8 @@ models = train_models_insee(
     "num_trips",
     df_activity,
     device=device,
-    input_size=input_size,
-    output_size=output_size,
+    input_size=INPUT_SIZE,
+    output_size=OUTPUT_SIZE,
     gammas=gammas,
     divergence=False,
 )
@@ -86,22 +98,22 @@ results = eval_models_insee(
     "num_trips",
     df_activity,
     device,
-    input_size=input_size,
-    output_size=output_size,
+    input_size=INPUT_SIZE,
+    output_size=OUTPUT_SIZE,
 )
 plot_forecasts_insee(
     results,
     "num_trips",
     df_activity,
     gammas,
-    input_size=input_size,
-    output_size=output_size,
+    input_size=INPUT_SIZE,
+    output_size=OUTPUT_SIZE,
 )
-error_insee(
+eval_models_insee(
     results,
     "num_trips",
     df_activity,
     device,
-    input_size=input_size,
-    output_size=output_size,
+    input_size=INPUT_SIZE,
+    output_size=OUTPUT_SIZE,
 )

1b856af0b1b4d5e3aeff5348642c806cabd2f39a
laurinemir <laurinemeier@gmail.com>
2025-03-26 12:52:53 +0000

    finaliser données S3

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
 import pandas as pd
 import torch
+import os
 from model.train_model import train_models_insee
 from model.eval_model import eval_models_insee, error_insee
 from model.forecast_model import plot_forecasts_insee
 import matplotlib.pyplot as plt
 import s3fs
+from dotenv import load_dotenv
 
 
-files = [
-    "taxi_data/yellow_tripdata_2025-01.parquet", "taxi_data/yellow_tripdata_2024-12.parquet",
-    "taxi_data/yellow_tripdata_2024-11.parquet", "taxi_data/yellow_tripdata_2024-10.parquet",
-    "taxi_data/yellow_tripdata_2024-09.parquet", "taxi_data/yellow_tripdata_2024-08.parquet",
-    "taxi_data/yellow_tripdata_2024-07.parquet", "taxi_data/yellow_tripdata_2024-06.parquet",
-    "taxi_data/yellow_tripdata_2024-05.parquet", "taxi_data/yellow_tripdata_2024-04.parquet",
-    "taxi_data/yellow_tripdata_2024-03.parquet", "taxi_data/yellow_tripdata_2024-02.parquet",
-    "taxi_data/yellow_tripdata_2024-01.parquet"
-]
+load_dotenv()
 
-MY_BUCKET = "laurinemir"
-CHEMIN_FICHIER = "taxi_data/yellow_tripdata_2024-01.parquet"
+
+MY_BUCKET = os.getenv("MY_BUCKET", "laurinemir")
+path = f"s3://{MY_BUCKET}/taxi_data/"
 
 fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": "https://minio.lab.sspcloud.fr"})
+files = fs.ls(path)
+
 dfs = []
 for file in files:
-    with fs.open(f"s3://{MY_BUCKET}/{file}") as f:
+    with fs.open(file) as f:
         df = pd.read_parquet(f)
         dfs.append(df)
 
 df = pd.concat(dfs, ignore_index=True)
-
-#df = pd.concat([pd.read_parquet("taxi_data/"+file) for file in file_list], ignore_index=True)
 print(df.head())
+
 #df = pd.read_csv("DS_ICA_CSV_FR/DS_ICA_data.csv", sep=";", encoding="utf-8")
 # df["TIME_PERIOD"] = pd.to_datetime(df["TIME_PERIOD"], format="%Y-%m")
+
+
 df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"], format="%Y-%m-%d %H:%M:%S")
 df["hour"] = df["tpep_pickup_datetime"].dt.floor("h")
 df_activity = df.groupby("hour").size().reset_index(name="num_trips")
 df_activity = df_activity[df_activity["num_trips"] >= 100]
 df_activity["hour"] = pd.to_datetime(df_activity["hour"])
 print(df_activity.head(n=10)) 
+
+
 # colonne = df.columns[0]  # colonne Activite
 # df_activity = df[
 #     (df[colonne] == "L")

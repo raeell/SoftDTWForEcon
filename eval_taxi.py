fix normalization metrics

eval_taxi.py
@@ -1,10 +1,10 @@
 """Evaluation script for taxi data."""
 
 import logging
+from pathlib import Path
 
 import torch
 from dotenv import load_dotenv
-from pathlib import Path
 
 from data.data_loader import DataLoaderS3
 from data.data_preprocessing import DataConfig
@@ -64,16 +64,16 @@ for path in dir_weights.glob("*gamma*.pt"):
         len(data_config.input_columns),
     )
     model.load_state_dict(torch.load(str(path)))
-    models.append(model)
-for model in dir_weights.glob("*MSE*.pt"):
+    models.append(model.to(device))
+for path in dir_weights.glob("*MSE*.pt"):
     model = MLP(
         data_config.input_size,
         training_config.hidden_size,
         data_config.output_size,
         len(data_config.input_columns),
     )
-    model.load_state_dict(torch.load)
-    models.append(model)
+    model.load_state_dict(torch.load(str(path)))
+    models.append(model.to(device))
 
 results = eval_models(
     models,
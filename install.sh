05097e899e02cd1577fa07b0c28b63e7f7aed901
laurinemir <laurinemeier@gmail.com>
2025-04-30 13:54:24 +0000

    leave Jupiter notebook from install

--- install.sh
+++ install.sh
@@ -4,7 +4,5 @@
 python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-# Create Jupyter kernel for venv
-python -m ipykernel install --user --name .venv
 # Create directory for plots
 mkdir plots
